3ae2a04c60ceedcd1fcc827dd979ff72eb813bdd
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2019-07-31 21:40:07 +0300

    Create Insertion Sort.cs

---
+++ Basic Sorting Algorithms/Insertion Sort.cs
@@ -0,0 +1,12 @@
+public void InsertionSort() { 
+  int inner, temp; 
+  for(int outer = 1; outer <= upper; outer++) { 
+    temp = arr[outer]; 
+    inner = outer; 
+    while(inner > 0 && arr[inner-1] >= temp) { 
+      arr[inner] = arr[inner-1]; 
+      inner -= 1; 
+    } 
+    arr[inner] = temp; 
+  } 
+}
